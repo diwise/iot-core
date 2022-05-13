08c0585aa5d5acfc2c4a08ea6bd58164ae391211
Mikael Nordberg <mikael.nordberg@masarinconsulting.se>
2022-05-13 13:35:46 +0200

    added constants for measurements

---
+++ pkg/measurements/measurements.go
@@ -0,0 +1,12 @@
+package measurements
+
+const (
+	Temperature string = "Temperature"
+	CO2 string = "CO2"
+	Humidity string = "Humidity"
+	Light string = "Light"
+	Motion string = "Motion"
+	BatteryLevel string = "BatteryLevel"
+	SnowHeight string = "SnowHeight"
+	Presence string = "Presence"	
+)
