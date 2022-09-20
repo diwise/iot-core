14a6b376d5ba7340abd2093e41acf0d76f6f68e7
Mas Dahlberg <mats.dahlberg@intraservice.goteborg.se>
2022-09-20 14:43:30 +0200

    soil constants

---
+++ pkg/glossary/w3org/constants.go
@@ -0,0 +1,8 @@
+package w3org
+
+const w3orgPrefix string = "http://purl.org/iot/vocab/m3-lite#S"
+
+const (
+	SoilHumidity        string = w3orgPrefix + "SoilHumidity"
+	SoilMoistureTension string = w3orgPrefix + "SoilMoistureTension"
+)
