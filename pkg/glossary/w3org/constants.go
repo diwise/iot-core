cfae8b9e03d2c1da523db46242e1ed9fb02a00c3
Mas Dahlberg <mats.dahlberg@intraservice.goteborg.se>
2022-09-22 07:21:37 +0200

    prefix corr

--- pkg/glossary/w3org/constants.go
+++ pkg/glossary/w3org/constants.go
@@ -1,6 +1,6 @@
 package w3org
 
-const w3orgPrefix string = "http://purl.org/iot/vocab/m3-lite#S"
+const w3orgPrefix string = "http://purl.org/iot/vocab/m3-lite#"
 
 const (
 	SoilHumidity        string = w3orgPrefix + "SoilHumidity"
