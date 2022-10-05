2814b5bde2ebd6a04fc90e6534fa6159b482fb9e
Mas Dahlberg <mats.dahlberg@intraservice.goteborg.se>
2022-10-05 16:27:21 +0200

    removed soil constants

--- pkg/glossary/w3org/constants.go
+++
@@ -1,8 +0,0 @@
-package w3org
-
-const w3orgPrefix string = "http://purl.org/iot/vocab/m3-lite#"
-
-const (
-	SoilHumidity        string = w3orgPrefix + "SoilHumidity"
-	SoilMoistureTension string = w3orgPrefix + "SoilMoistureTension"
-)
