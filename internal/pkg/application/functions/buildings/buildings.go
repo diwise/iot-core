60d75f01ad66118314232d96d37c79f9a5d1de4f
shestyf <sofia.styf@masarinconsulting.se>
2023-05-08 11:30:31 +0200

    building function now handles power and energy

--- internal/pkg/application/functions/buildings/buildings.go
+++ internal/pkg/application/functions/buildings/buildings.go
@@ -2,6 +2,7 @@ package buildings
 
 import (
 	"context"
+	"math"
 
 	"github.com/diwise/iot-core/pkg/lwm2m"
 	"github.com/diwise/iot-core/pkg/messaging/events"
@@ -13,6 +14,9 @@ const (
 
 type Building interface {
 	Handle(context.Context, *events.MessageAccepted, func(string, float64)) (bool, error)
+
+	CurrentPower() float64
+	CurrentEnergy() float64
 }
 
 func New() Building {
@@ -29,7 +33,44 @@ func (b *building) Handle(ctx context.Context, e *events.MessageAccepted, onchan
 		return false, nil
 	}
 
-	//const SensorValue string = "5700"
+	const SensorValue string = "5700"
+	value, ok := e.GetFloat64(SensorValue)
+
+	if ok {
+		if e.BaseNameMatches(lwm2m.Power) {
+			previousValue := b.Power
+			b.Power = value
+
+			if hasChanged(previousValue, value) {
+				onchange("power", value)
+				return true, nil
+			}
+		} else if e.BaseNameMatches(lwm2m.Energy) {
+			previousValue := b.Energy
+			b.Energy = value
+
+			if hasChanged(previousValue, value) {
+				onchange("energy", value)
+				return true, nil
+			}
+		}
+	}
 
 	return false, nil
+}
+
+func hasChanged(previousLevel, newLevel float64) bool {
+	return isNotZero(newLevel - previousLevel)
+}
+
+func isNotZero(value float64) bool {
+	return (math.Abs(value) >= 0.001)
+}
+
+func (b *building) CurrentPower() float64 {
+	return b.Power
+}
+
+func (b *building) CurrentEnergy() float64 {
+	return b.Energy
 }
--- internal/pkg/application/functions/buildings/buildings_test.go
+++ internal/pkg/application/functions/buildings/buildings_test.go
@@ -1,1 +1,47 @@
 package buildings
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/diwise/iot-core/pkg/lwm2m"
+	"github.com/diwise/iot-core/pkg/messaging/events"
+	"github.com/matryer/is"
+)
+
+func TestBuildingPower(t *testing.T) {
+	is := is.New(t)
+
+	b := New()
+
+	b.Handle(context.Background(), newValue(lwm2m.Power, 22322.0), func(s string, f float64) {})
+	is.Equal(b.CurrentPower(), 22322.0)
+}
+
+func TestBuildingEnergy(t *testing.T) {
+	is := is.New(t)
+
+	b := New()
+
+	b.Handle(context.Background(), newValue(lwm2m.Energy, 33333.0), func(s string, f float64) {})
+	is.Equal(b.CurrentEnergy(), 33333.0)
+}
+
+func newValue(baseName string, value float64) *events.MessageAccepted {
+	e := &events.MessageAccepted{}
+	json.Unmarshal([]byte(
+		fmt.Sprintf(messageJSONFormat, baseName, value),
+	), e)
+	return e
+}
+
+const messageJSONFormat string = `{
+	"sensorID":"sensorID",
+	"pack":[
+		{"bn":"%s","bt":1675801037,"n":"0","vs":"testId"},
+		{"n":"5700","u":"m","v":%f}
+	],
+	"timestamp":"2023-02-07T20:17:17.312028Z"
+}`
