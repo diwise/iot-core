5206510189289b87b38b7ad631fc8674ae3d7314
shestyf <sofia.styf@masarinconsulting.se>
2024-02-08 14:50:42 +0100

    added timestamp to state

--- internal/pkg/application/functions/states/states.go
+++ internal/pkg/application/functions/states/states.go
@@ -25,7 +25,8 @@ func New(v float64) State {
 }
 
 type state struct {
-	State_ bool `json:"state"`
+	Timestamp string `json:"timestamp"`
+	State_    bool   `json:"state"`
 }
 
 func (t *state) Handle(ctx context.Context, e *events.MessageAccepted, onchange func(prop string, value float64, ts time.Time) error) (bool, error) {
@@ -43,6 +44,8 @@ func (t *state) Handle(ctx context.Context, e *events.MessageAccepted, onchange 
 
 	if stateOk && timeOk && r.BoolValue != nil {
 		t.State_ = *r.BoolValue
+		t.Timestamp = ts.Format(time.RFC3339)
+
 		stateValue := map[bool]float64{true: 1, false: 0}
 
 		// This does not actually check if state has changed. It simply sets "state" to the mapped value of t.State_
