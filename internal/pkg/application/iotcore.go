3661364542e71ece8a1f6015bcde7327b68d481d
Mikael Nordberg <mikael.nordberg@outlook.com>
2026-01-23 10:16:25 +0100

    minor fixes

--- internal/pkg/application/iotcore.go
+++ internal/pkg/application/iotcore.go
@@ -90,7 +90,7 @@ func (a *app) MessageReceived(ctx context.Context, msg events.MessageReceived) (
 		events.Source(device.Source()),
 		events.Tenant(device.Tenant()))
 
-	switch msg.ObjectID() {			
+	switch msg.ObjectID() {
 	case "3200":
 		decs = append(decs, decorators.DigitalInput(ctx, decorators.GetNumberOfTrueValues(ctx, a.measurementsClient, device.ID())))
 	}
